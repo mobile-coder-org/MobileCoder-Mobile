a0b9679247710c722035e37f029e44e1ee9aed8f
Crystal Lee <crys@nyu.edu>
2020-12-06 22:15:09 -0500

    add react-native-keyboard-aware-scroll-view

--- MobileCoder/screens/SignupScreen.js
+++ MobileCoder/screens/SignupScreen.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {View, StyleSheet, Text, TextInput, TouchableOpacity, Alert} from 'react-native'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 //import * as firebase from 'firebase'
 import firebase from '../environment/config'
 import UserService from '../services/UserService'
@@ -41,61 +42,68 @@ export default class SignupScreen extends React.Component {
 
     render() {
         return (
-            <View style={styles.container}>
-                <Text style={styles.text}>Create an account</Text>
-
-                <View style={styles.error}>
-                    {this.state.error && <Text style={{color: '#fff'}}>{this.state.error}</Text>}
-                </View>
-
-                <View style={styles.form}>
-                    <View>
-                        <Text style={styles.title}>Name</Text>
-                        <TextInput 
-                            keyboardAppearance="dark"
-                            style={styles.input} 
-                            autoCapitalize="none" 
-                            onChangeText={name => this.setState({name})}
-                            value={this.state.name}
-                        ></TextInput>
+            <KeyboardAwareScrollView
+                resetScrollToCoords={{ x: 0, y: 0 }}
+                contentContainerStyle={styles.container}
+                scrollEnabled={false}
+            >
+                <View style={styles.container}>
+                    <Text style={styles.text}>Create an account</Text>
+                    <View style={styles.error}>
+                        {this.state.error && <Text style={{color: '#fff'}}>{this.state.error}</Text>}
                     </View>
-                    <View style={{marginTop: 32}}>
-                        <Text style={styles.title}>Email Address</Text>
-                        <TextInput 
-                            keyboardAppearance="dark"
-                            style={styles.input} 
-                            autoCapitalize="none" 
-                            onChangeText={email => this.setState({email})}
-                            value={this.state.email}
-                        ></TextInput>
-                    </View>
-
-                    <View style={{marginTop: 32}}>
+                    <View style={styles.form}>
                         <View>
-                            <Text style={styles.title}>Password</Text>
+                            <Text style={styles.title}>Name</Text>
                             <TextInput 
                                 keyboardAppearance="dark"
                                 style={styles.input} 
-                                secureTextEntry={true}
-                                autoCapitalize="none"
-                                onChangeText={password => this.setState({password})}
-                                value={this.state.password}
+                                autoCapitalize="none" 
+                                onChangeText={name => this.setState({name})}
+                                value={this.state.name}
+                                ref={(r) => { this._textInputRef = r; }}
                             ></TextInput>
                         </View>
+                        <View style={{marginTop: 32}}>
+                            <Text style={styles.title}>Email Address</Text>
+                            <TextInput 
+                                keyboardAppearance="dark"
+                                style={styles.input} 
+                                autoCapitalize="none" 
+                                onChangeText={email => this.setState({email})}
+                                value={this.state.email}
+                                ref={(r) => { this._textInputRef = r; }}
+                            ></TextInput>
+                        </View>
+                        <View style={{marginTop: 32}}>
+                            <View>
+                                <Text style={styles.title}>Password</Text>
+                                <TextInput 
+                                    keyboardAppearance="dark"
+                                    style={styles.input} 
+                                    secureTextEntry={true}
+                                    autoCapitalize="none"
+                                    onChangeText={password => this.setState({password})}
+                                    value={this.state.password}
+                                    ref={(r) => { this._textInputRef = r; }}
+                                ></TextInput>
+                            </View>
+                        </View>
+                        
                     </View>
+
+                    <TouchableOpacity style={styles.button} onPress={this.handleSignup}>
+                        <Text style= {{color: '#fff', fontWeight: '600'}}>Sign up</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity 
+                        style={{alignSelf:'center', marginTop: 32}}
+                        onPress={() => this.props.navigation.navigate("Login")}
+                    >
+                        <Text style={{color: '#fff', fontSize: 13}}>Already have an account? <Text>Log in</Text></Text>
+                    </TouchableOpacity>
                 </View>
-
-                <TouchableOpacity style={styles.button} onPress={this.handleSignup}>
-                    <Text style= {{color: '#fff', fontWeight: '600'}}>Sign up</Text>
-                </TouchableOpacity>
-
-                <TouchableOpacity 
-                    style={{alignSelf:'center', marginTop: 32}}
-                    onPress={() => this.props.navigation.navigate("Login")}
-                >
-                    <Text style={{color: '#fff', fontSize: 13}}>Already have an account? <Text>Log in</Text></Text>
-                </TouchableOpacity>
-            </View>
+                </KeyboardAwareScrollView>
         );
     }   
 }
